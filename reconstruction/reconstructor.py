eec28885c672fb25212d4f558efbb4abce2de9c1
Pablo Arcos López <pablo.arcos@goldenspear.com>
2022-06-01 21:13:40 +0200

    Separated image matcher

--- reconstruction/reconstructor.py
+++ reconstruction/reconstructor.py
@@ -1,7 +1,7 @@
 from typing import Tuple
-from pydantic import BaseModel
 import open3d as o3d
 from reconstruction.dataset import Dataset
+from reconstruction.config import Config
 import numpy as np
 import cv2 as cv
 from itertools import accumulate
@@ -14,26 +14,8 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
 
-class ReconstructorConfig(BaseModel):
-    device: str
-    camera_intrinsic: np.ndarray
-    voxel_size: float
-    block_resolution: int
-    block_count: int
-    depth_scale: float
-    depth_max: float
-    depth_min: float
-    fragment_size: int = 8
-    """
-    The number of fragments to use for each block."""
-    debug: bool = False
-
-    class Config:
-        arbitrary_types_allowed = True
-
-
 class ClassicReconstructor():
-    def __init__(self, config: ReconstructorConfig):
+    def __init__(self, config: Config):
         self.config = config
 
         self.feature_extractor = cv.SIFT_create()
@@ -109,6 +91,7 @@ class ClassicReconstructor():
 
         return self.volume
 
+
     def _register_pair(self, source: o3d.t.geometry.PointCloud, target: o3d.t.geometry.PointCloud, initial_transform: np.ndarray) -> np.ndarray:
         """
         Register a point cloud to another using ICP.
@@ -134,79 +117,6 @@ class ClassicReconstructor():
                                                               initial_transform, estimation)
 
         return result.transformation.numpy()
-
-    def _match_image_pair(self, source: np.ndarray, target: np.ndarray, initial_transform: np.ndarray) -> np.ndarray:
-        """
-        Match two images using RootSIFT.
-
-        Returns:
-            Estimated transformation.
-        """
-        # Based on https://docs.opencv.org/4.x/dc/dc3/tutorial_py_matcher.html
-
-        source_keypoints, source_descriptors = self._detect_keypoints(source)
-        target_keypoints, target_descriptors = self._detect_keypoints(target)
-
-        # Match keypoints
-        FLANN_INDEX_KDTREE = 1
-        index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-        search_params = dict(checks=50)
-        flann = cv.FlannBasedMatcher(index_params, search_params)
-        matches = flann.knnMatch(source_descriptors, target_descriptors, k=2)
-
-        # Lowe's ratio test.
-        good = []
-        for m, n in matches:
-            if m.distance < 0.7 * n.distance:
-                good.append(m)
-
-        src_pts = np.float32([source_keypoints[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-        dst_pts = np.float32([target_keypoints[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
-
-        # We can only run 5-point algorithm if we have at least 5 matches
-        if src_pts.shape[0] < 5:
-            return initial_transform
-
-        # We can build the essential matrix instead of the fundamental 
-        # because we have the camera intrinsics
-        essential, mask = cv.findEssentialMat(
-            src_pts, dst_pts, self.camera_intrinsic.numpy(), cv.RANSAC)
-        rotation1, rotation2, translation = cv.decomposeEssentialMat(essential)
-
-        # Build final transform
-        # TODO The translation is a unit vector, it's just the direction of motion 
-        translation *= np.linalg.norm(initial_transform[:3, 3])
-        relative_transform = np.array(initial_transform, copy=True)
-        relative_transform[:3, :3] = rotation1
-        relative_transform[:3, 3:3] = translation
-
-        return relative_transform
-
-
-    def _detect_keypoints(self, image: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-        """
-        Detects keypoints in an image.
-
-        Args:
-            image: RGB image.
-        Returns:
-            Keypoints and descriptors.
-        """
-        # Implements RootSIFT (https://www.robots.ox.ac.uk/~vgg/publications/2012/Arandjelovic12/arandjelovic12.pdf)
-        eps = 1e-7
-
-        (kps, descs) = self.feature_extractor.detectAndCompute(image, None)
-        
-        if len(kps) == 0:
-            return ([], None)
-
-        # apply the Hellinger kernel by first L1-normalizing and taking the
-        # square-root
-        descs /= (descs.sum(axis=1, keepdims=True) + eps)
-        descs = np.sqrt(descs)
-
-        return (kps, descs)
-
 
     def _extract_point_cloud(self, depth_file: str, color_file: str) -> o3d.t.geometry.PointCloud:
         """
